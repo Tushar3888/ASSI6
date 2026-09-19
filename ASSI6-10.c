35848190644c1f0ccb2499c96d90a0c45e7d92d8
Tushar3888 <TUSHAR.30008@stu.upes.ac.in>
2026-09-19 14:10:44 +0530

    Add Pascal's triangle implementation in C

---
+++ ASSI6-10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, space;
+    long long num;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        /* Print spaces */
+        for (space = 0; space < n - i - 1; space++)
+        {
+            printf("  ");
+        }
+
+        num = 1;
+
+        /* Print numbers in each row */
+        for (j = 0; j <= i; j++)
+        {
+            printf("%4lld", num);
+            num = num * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
